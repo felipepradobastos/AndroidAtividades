46f65650e7d0320543703ad5fdda473f68253004
Felipe Bastos <felipepradobastos@gmail.com>
2019-10-08 10:41:52 -0300

    Adicionando activity 169

---
+++ MenuActivityFelipeB/app/src/main/java/ffs/company/menuactivityfelipeb/CadAlunoActivity.java
@@ -0,0 +1,13 @@
+package ffs.company.menuactivityfelipeb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_aluno);
+    }
+}
